7a66fe6e22961d1f9b6018aad80cb3ddccf6855f
Tomasz Niedźwiecki <tomaszniedz1988@outlook.com>
2024-01-14 10:25:59 +0100

    Praca domowa lekcja 4

--- ChellengeApp/Program.cs
+++ ChellengeApp/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿bool isWoman = true;
+var age = 18;
+string name = "EWA";
+
+if (isWoman == true && age < 30)
+{
+     Console.WriteLine("Kobieta ponizej 30 lat");
+}
+else if (name == "EWA" && age == 30)
+{
+     Console.WriteLine("Ewa lat 30");
+}
+else if (isWoman == false && age < 18)
+{
+     Console.WriteLine("Niepełnoletni meżczyzna");
+}
+else
+{
+     Console.WriteLine("Ktoś inny");
+}
